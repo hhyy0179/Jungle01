205d85eb53f608a44f04f436ac564f118f8c9986
hhyy0179 <rharhazbvl@gmail.com>
2024-03-19 11:52:09 +0900

    Feat: Install package

---
+++ app.py
@@ -0,0 +1,23 @@
+from flask import Flask, render_template,jsonify, request
+app = Flask(__name__)
+
+import requests
+
+
+from pymongo import MongoClient
+client = MongoClient('mongodb://test:test@localhost',27017) #원격 DB접속
+#client = MongoClient('localhost',27017)
+db = client.dbjungle
+
+##URL 별로 함수명이 같거나,
+## route('/') 등의 주소가 같으면 안됨.
+
+
+def home():
+    return render_template('alonememo.html')
+
+
+
+if __name__ == '__main__':
+    
+    #app.run('0.0.0.0', port=5001, debug = True)
