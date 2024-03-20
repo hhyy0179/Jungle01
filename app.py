60a3f6515a8346ed1c9168abe93a64cd9dcfbf48
Joshooa <klasrptmxm@gmail.com>
2024-03-20 21:46:58 +0900

    층별 선택 기능 구현완료

--- app.py
+++ app.py
@@ -1,3 +1,7 @@
+import json
+from bson import ObjectId
+from flask.json.provider import JSONProvider
+
 from flask import (
     Flask,
     flash,
@@ -20,6 +24,31 @@ from pymongo import MongoClient
 
 client = MongoClient("localhost", 27017)
 db = client.dbrefrigerator
+
+
+######################################################################################
+# ObjectId 타입으로 되어있는 _id 필드는 Flask 의 jsonify 호출시 문제가 된다.
+# 이를 처리하기 위해서 기본 JsonEncoder 가 아닌 custom encoder 를 사용한다.
+# Custom encoder 는 다른 부분은 모두 기본 encoder 에 동작을 위임하고 ObjectId 타입만 직접 처리한다.
+class CustomJSONEncoder(json.JSONEncoder):
+    def default(self, o):
+        if isinstance(o, ObjectId):
+            return str(o)
+        return json.JSONEncoder.default(self, o)
+
+
+class CustomJSONProvider(JSONProvider):
+    def dumps(self, obj, **kwargs):
+        return json.dumps(obj, **kwargs, cls=CustomJSONEncoder)
+
+    def loads(self, s, **kwargs):
+        return json.loads(s, **kwargs)
+
+
+# 위에 정의되 custom encoder 를 사용하게끔 설정한다.
+app.json = CustomJSONProvider(app)
+
+# #####################################################################################
 
 
 ## HTML을 주는 부분
@@ -101,13 +130,12 @@ def logout():
 def find():
     # foodList = show_foods()
     token_receive = request.cookies.get("mytoken")
-    foodList = show_foods()
     try:
         payload = jwt.decode(
             token_receive, SECRET_KEY, algorithms=["HS256"]
         )  # token디코딩합니다.
         userinfo = db.refrigerator.find_one({"user_id": payload["user_id"]}, {"_id": 0})
-        print(userinfo)
+        # print(userinfo)
         floors = [i for i in range(1, 23)]
         roomFloor = int(userinfo.get("room_number", "1")) // 100
         foods = list(
@@ -115,7 +143,7 @@ def find():
                 {"refrigerator_floor": roomFloor, "food_count": {"$ne": 0}}
             )
         )
-
+        # print(foods[0])
         return render_template(
             "index.html",
             user_info=userinfo,
@@ -160,7 +188,7 @@ def show_foods():
     try:
         # 해당 층의 음식 정보만 필터링
         foods_cursor = db.refrigerator.find(
-            {"food_count": {"$ne": 0}, "refrigerator_floor": int(floor)}, {"_id": 0}
+            {"food_count": {"$ne": 0}, "refrigerator_floor": int(floor)}
         ).sort(
             [("expiration_year", -1), ("expiration_month", -1), ("expiration_day", -1)]
         )
@@ -173,6 +201,24 @@ def show_foods():
 @app.route("/registration")
 def registration():
     return render_template("registration.html")
+
+
+# 음식 신청하기 api (html카드 생성에서만 작동)
+@app.route("/api/apply", methods=["POST"])
+def apply():
+    apply_foodid_receive = request.form["apply_foodid_give"]
+    food = db.refrigerator.find_one({"_id": ObjectId(apply_foodid_receive)})
+    print("현재 음식 개수", food["food_count"])
+
+    new_food_count = food["food_count"] - 1
+    result = db.refrigerator.update_one(
+        {"_id": food["_id"]}, {"$set": {"food_count": new_food_count}}
+    )
+
+    if result.modified_count == 1:
+        return jsonify({"result": "success"})
+    else:
+        return jsonify({"result": "failure"})
 
 
 # # 음식 등록하기 api
